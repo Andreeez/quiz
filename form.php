baa756277abb395fe59d38c0f07b1c8028478e0a
Andreeez <32762098+Andreeez@users.noreply.github.com>
2018-02-20 18:43:58 +0100

    Uppdatering source för bilder. Nu funkar sidan på liverservern.

--- form.php
+++ form.php
@@ -5,13 +5,13 @@
 
                     <input type="radio" name="question1-answer" id="question1A1" value="1"/>
                     <label for="question1A1">1 - Danny Saucedo</label>
-                    <label for="question2A1"><img src="/picture/danny.jpg"></label>
+                    <label for="question2A1"><img src="./picture/danny.jpg"></label>
                     <input type="radio" name="question1-answer" id="question1AX" value="X"/>
                     <label for="question1AX">X - Eric Saade</label>
-                    <label for="question2A1"><img src="/picture/eric.jpg"></label>
+                    <label for="question2A1"><img src="./picture/eric.jpg"></label>
                     <input type="radio" name="question1-answer" id="question1A2" value="2"/>
                     <label for="question1A2">2 - Anton Ewald</label>
-                    <label for="question2A1"><img src="/picture/anton.jpg"></label>
+                    <label for="question2A1"><img src="./picture/anton.jpg"></label>
 
                 </div>
 
@@ -20,39 +20,39 @@
 
                     <input type="radio" name="question2-answer" id="question2A1" value="1"/>
                     <label for="question2A1">1 - Måns Zelmerlöv</label>
-                    <label for="question2A1"><img src="/picture/mans.jpg"></label>
+                    <label for="question2A1"><img src="./picture/mans.jpg"></label>
                     <input type="radio" name="question2-answer" id="question2AX" value="X"/>
                     <label for="question2AX">X - Robin Stjernberg</label>
-                    <label for="question2A1"><img src="/picture/robin.jpg"></label>
+                    <label for="question2A1"><img src="./picture/robin.jpg"></label>
                     <input type="radio" name="question2-answer" id="question2A2" value="2"/>
                     <label for="question2A2">2 - Danny Saucedo</label>
-                    <label for="question2A1"><img src="/picture/danny.jpg"></label>
+                    <label for="question2A1"><img src="./picture/danny.jpg"></label>
                 </div>
                 <div>
                     <h3>Vem sjöng låten Save Me?</h3>
 
                     <input type="radio" name="question3-answer" id="question3A1" value="1"/>
                     <label for="question2A1">1 - Molly Pettersson Hammar</label>
-                    <label for="question2A1"><img src="/picture/mollypettersson.jpg"></label>
+                    <label for="question2A1"><img src="./picture/mollypettersson.jpg"></label>
                     <input type="radio" name="question3-answer" id="question3AX" value="X"/>
                     <label for="question2AX">X - ISA</label>
-                    <label for="question2A1"><img src="/picture/isa.jpg"></label>
+                    <label for="question2A1"><img src="./picture/isa.jpg"></label>
                     <input type="radio" name="question3-answer" id="question3A2" value="2"/>
                     <label for="question2A2">2 - Wiktoria</label>
-                    <label for="question2A1"><img src="/picture/wiktoria.jpg"></label>
+                    <label for="question2A1"><img src="./picture/wiktoria.jpg"></label>
                 </div>
                 <div>
                     <h3>Vem sjöng låten Håll om mig Hårt?</h3>
 
                     <input type="radio" name="question4-answer" id="question4A1" value="1"/>
                     <label for="question2A1">1 - Panetoz</label>
-                    <label for="question2A1"><img src="/picture/panetoz.jpg"></label>
+                    <label for="question2A1"><img src="./picture/panetoz.jpg"></label>
                     <input type="radio" name="question4-answer" id="question4AX" value="X"/>
                     <label for="question2AX">X - Samir och Viktor</label>
-                    <label for="question2A1"><img src="/picture/samiroviktor.jpg"></label>
+                    <label for="question2A1"><img src="./picture/samiroviktor.jpg"></label>
                     <input type="radio" name="question4-answer" id="question4A2" value="2"/>
                     <label for="question2A2">2 - De vet du</label>
-                    <label for="question2A1"><img src="/picture/devetdu.jpg"></label>
+                    <label for="question2A1"><img src="./picture/devetdu.jpg"></label>
 
                 </div>
                 <div>
@@ -60,78 +60,78 @@
 
                     <input type="radio" name="question5-answer" id="question5A1" value="1"/>
                     <label for="question2A1">1 - Carola</label>
-                    <label for="question2A1"><img src="/picture/carola.jpg"></label>
+                    <label for="question2A1"><img src="./picture/carola.jpg"></label>
                     <input type="radio" name="question5-answer" id="question5AX" value="X"/>
                     <label for="question2AX">X - Ace Wilder</label>
-                    <label for="question2A1"><img src="/picture/acewilder.jpg"></label>
+                    <label for="question2A1"><img src="./picture/acewilder.jpg"></label>
                     <input type="radio" name="question5-answer" id="question5A2" value="2"/>
                     <label for="question2A2">2 - Molly Sanden</label>
-                    <label for="question2A1"><img src="/picture/mollysanden.jpg"></label>
+                    <label for="question2A1"><img src="./picture/mollysanden.jpg"></label>
                 </div>
                 <div>
                     <h3>Vem sjöng låten Genom Eld och Vatten</h3>
 
                     <input type="radio" name="question6-answer" id="question6A1" value="1"/>
                     <label for="question2A1">1 - Sarek</label>
-                    <label for="question2A1"><img src="/picture/sarek.jpg"></label>
+                    <label for="question2A1"><img src="./picture/sarek.jpg"></label>
                     <input type="radio" name="question6-answer" id="question6AX" value="X"/>
                     <label for="question2AX">X - Roger Pontare</label>
-                    <label for="question2A1"><img src="/picture/rogerpontare.jpg"></label>
+                    <label for="question2A1"><img src="./picture/rogerpontare.jpg"></label>
                     <input type="radio" name="question6-answer" id="question6A2" value="2"/>
                     <label for="question2A2">2 - Linda Bengtzing</label>
-                    <label for="question2A1"><img src="/picture/lindabengtzing.jpg"></label>
+                    <label for="question2A1"><img src="./picture/lindabengtzing.jpg"></label>
                 </div>
                 <div>
                     <h3>Vem sjöng låten Kiss you Goodbye</h3>
 
                     <input type="radio" name="question7-answer" id="question7A1" value="1"/>
                     <label for="question2A1">1 - Anton Hagman</label>
-                    <label for="question2A1"><img src="/picture/antonhagman.jpg"></label>
+                    <label for="question2A1"><img src="./picture/antonhagman.jpg"></label>
                     <input type="radio" name="question7-answer" id="question7AX" value="X"/>
                     <label for="question2AX">X - FO och O</label>
-                    <label for="question2A1"><img src="/picture/foocho.jpg"></label>
+                    <label for="question2A1"><img src="./picture/foocho.jpg"></label>
                     <input type="radio" name="question7-answer" id="question7A2" value="2"/>
                     <label for="question2A2">2 - Benjamin Ingrosso</label>
-                    <label for="question2A1"><img src="/picture/benjaminingrosso.jpg"></label>
+                    <label for="question2A1"><img src="./picture/benjaminingrosso.jpg"></label>
                 </div>
                 <div>
                     <h3>Vem sjöng låten Building it Up</h3>
 
                     <input type="radio" name="question8-answer" id="question8A1" value="1"/>
                     <label for="question2A1">1 - Oscar Zia</label>
-                    <label for="question2A1"><img src="/picture/oscarzia.jpg"></label>
+                    <label for="question2A1"><img src="./picture/oscarzia.jpg"></label>
                     <input type="radio" name="question8-answer" id="question8AX" value="X"/>
                     <label for="question2AX">X - Anton Ewald</label>
-                    <label for="question2A1"><img src="/picture/anton.jpg"></label>
+                    <label for="question2A1"><img src="./picture/anton.jpg"></label>
                     <input type="radio" name="question8-answer" id="question8A2" value="2"/>
                     <label for="question2A2">2 - JTR</label>
-                    <label for="question2A1"><img src="/picture/jtr.jpg"></label>
+                    <label for="question2A1"><img src="./picture/jtr.jpg"></label>
                 </div>
                 <div>
                     <h3>Vem sjöng låten Constellation Prize</h3>
 
                     <input type="radio" name="question9-answer" id="question9A1" value="1"/>
                     <label for="question2A1">1 - Robin Stjernberg</label>
-                    <label for="question2A1"><img src="/picture/robin.jpg"></label>
+                    <label for="question2A1"><img src="./picture/robin.jpg"></label>
                     <input type="radio" name="question9-answer" id="question9AX" value="X"/>
                     <label for="question2AX">X - Måns Zelmerlöv</label>
-                    <label for="question2A1"><img src="/picture/mans.jpg"></label>
+                    <label for="question2A1"><img src="./picture/mans.jpg"></label>
                     <input type="radio" name="question9-answer" id="question9A2" value="2"/>
                     <label for="question2A2">2 - Robin Bengtsson</label>
-                    <label for="question2A1"><img src="/picture/robinbengtsson.jpg"></label>
+                    <label for="question2A1"><img src="./picture/robinbengtsson.jpg"></label>
                 </div>
                 <div>
                     <h3>Vem sjöng låten Undo</h3>
 
                     <input type="radio" name="question10-answer" id="question10A1" value="1"/>
                     <label for="question2A1">1 - Molly Sanden</label>
-                    <label for="question2A1"><img src="/picture/mollysanden.jpg"></label>
+                    <label for="question2A1"><img src="./picture/mollysanden.jpg"></label>
                     <input type="radio" name="question10-answer" id="question10AX" value="X"/>
                     <label for="question2AX">X - Sanna Nielsen</label>
-                    <label for="question2A1"><img src="/picture/sannanielsen.jpg"></label>
+                    <label for="question2A1"><img src="./picture/sannanielsen.jpg"></label>
                     <input type="radio" name="question10-answer" id="question10A2" value="2"/>
                     <label for="question2A2">2 - Linda Bengtzing</label>
-                    <label for="question2A1"><img src="/picture/lindabengtzing.jpg"></label>
+                    <label for="question2A1"><img src="./picture/lindabengtzing.jpg"></label>
                 </div>
 
                     <br><br><br>
