660e955c46563cd7643be22a32bf52c542991d7a
Andreeez <32762098+Andreeez@users.noreply.github.com>
2018-02-20 18:29:18 +0100

    Quiz funkar. Nu ska det byggas om till klasser och smidigare lösning

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <link href="style.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <h1>Mello-quiz Vem sjöng Låten?</h1>
+        <?php include 'form.php' ?>
+        <?php include 'function.php' ?>
+        
+    </body>
+</html>
